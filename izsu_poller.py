be1814cf969697f43dc50cc1693b36dc77f39463
coskukaraman-hub <coskukaraman@gmail.com>
2026-09-15 15:13:40 +0300

    Update izsu_poller.py

--- izsu_poller.py
+++ izsu_poller.py
@@ -16,7 +16,7 @@ API_URL = "https://openapi.izmir.bel.tr/api/izsu/arizakaynaklisukesintileri"
 STATE_FILE = "state.json"
 
 # TAKIP EDILECEK ILCE/MAHALLE ANAHTAR KELIMELERI - kendi bolgene gore duzenle
-TAKIP_LISTESI = ["Bornova", "Karsiyaka"]
+TAKIP_LISTESI = ["Bornova", "Karsiyaka", "Gulyaka", "Reis", "Refet Bele", "Karabaglar"]
 
 # ntfy.sh konu adi - kendine ozel, tahmin edilemez bir isim sec
 NTFY_TOPIC = "izsu-kesinti-XXXXXX"
